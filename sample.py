6937f2a20a53e80b007d1a324b8c61709c51aef0
Athisruth <athisruthmsatheesh98@gmail.com>
2025-03-14 15:44:54 +0530

    "added file names"

--- sample.py
+++ sample.py
@@ -1,8 +1,11 @@
 import os
 import difflib
 
+
 originalFilePath = r"C:\Users\LENOVO\Desktop\prizmora\finding_edited_code\org.py"
 backupFilePath = "backup_file.py"
+original_Basename = os.path.basename(originalFilePath)
+backup_Basename = os.path.basename(backupFilePath)
 if os.path.exists(originalFilePath):
     with open(originalFilePath,"r") as f:
         original_content = f.readlines()
@@ -15,7 +18,7 @@ if os.path.exists(originalFilePath):
                 print(f"editing file is modified..")
                 for index in range(len(original_content)):
                     if original_content[index]!=backup_File_Content[index]:
-                        print(f"line :{index+1} of original_content is changed from {backup_File_Content[index]} to {original_content[index]}")
+                        print(f"line :{index+1} of {original_Basename} is changed from {backup_File_Content[index].strip()} to {original_content[index].strip()} in backupfile: {backup_Basename}")
 
     else:
         with open(backupFilePath, "w") as g:
